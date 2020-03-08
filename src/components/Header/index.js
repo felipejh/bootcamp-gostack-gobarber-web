f466bb77313bad80508553caa59f6ef8fabdaa19
felipe.hoffmann <felipe.hoffmann@gmail.com>
2020-03-08 15:39:24 -0300

    Listando agendamentos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
+
 import logo from '~/assets/logo_purple.svg';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +10,8 @@ import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,12 +24,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Felipe Hoffmann</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Felipe Hoffmann"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
